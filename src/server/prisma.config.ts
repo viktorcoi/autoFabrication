11659b6e3696d48d71ee949867f5c7de3ef00e16
viktorcoi <marandin5913@mail.ru>
2026-04-27 22:35:31 +0300

    Users 100%

--- src/server/prisma.config.ts
+++ src/server/prisma.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
 		seed: "tsx prisma/seed.ts",
 	},
 	datasource: {
-		url: process.env.DATABASE_URL ?? "file:./dev.db",
+		url:
+			process.env.DATABASE_URL
+			?? "postgresql://postgres:postgres@localhost:5432/autofabrication?schema=public",
 	},
 });
