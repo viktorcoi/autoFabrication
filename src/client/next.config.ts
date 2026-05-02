69a5f9a92b3949beea62d71ca0f8e8cc628f0c0c
viktorcoi <marandin5913@mail.ru>
2026-05-02 12:54:33 +0300

    update limits

--- src/client/next.config.ts
+++ src/client/next.config.ts
@@ -5,7 +5,7 @@ const apiProxyBaseUrl = process.env.API_INTERNAL_URL ?? process.env.NEXT_PUBLIC_
 const nextConfig: NextConfig = {
 	reactStrictMode: false,
 	experimental: {
-		proxyClientMaxBodySize: "600mb",
+		proxyClientMaxBodySize: "300mb",
 		webpackMemoryOptimizations: true,
 	},
 	async rewrites() {
